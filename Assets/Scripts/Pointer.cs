c84dbc38d695f449fee8c23d398d103a78afb83d
OverHome <OverHome@users.noreply.github.com>
2023-04-23 23:41:23 +0300

    pointers

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Pointer : MonoBehaviour
+{
+    [SerializeField] public string TrigerValueName;
+    [SerializeField] public int TrigerValue;
+    
+    [SerializeField] public string FinTrigerValueName;
+    [SerializeField] public int FinTrigerValue;
+
+    private SpriteRenderer sr;
+    private bool Ispok;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        Ispok = false;
+    }
+
+    private void Update()
+    {
+        if (PlayerPrefs.GetInt(FinTrigerValueName) == FinTrigerValue)
+        {
+            Ispok = false;
+        }
+        else if(PlayerPrefs.GetInt(TrigerValueName) == TrigerValue)
+        {
+            Ispok = true;
+        }
+
+        if (Ispok)
+        {
+            sr.color = new Color(255, 255, 255, 255);
+        }
+        else
+        {
+            sr.color = new Color(255, 255, 255, 0);
+        }
+
+
+    }
+    
+}
